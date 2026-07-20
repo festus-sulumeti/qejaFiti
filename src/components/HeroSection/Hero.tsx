bf4512ddd55bb9ab89da89ee0c262ecf43286998
festus-sulumeti <sulumetifestus@outlook.com>
2026-07-20 08:43:56 +0300

    chore(Path): Added the route path in get stated button section

--- src/components/HeroSection/Hero.tsx
+++ src/components/HeroSection/Hero.tsx
@@ -1,4 +1,5 @@
 import { ArrowUpRight } from "lucide-react";
+import { Link } from "react-router-dom"
 import { Button } from "@/components/ui/button";
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -39,7 +40,9 @@ export default function Hero() {
 
             <Button className="group not-disabled:inset-shadow-none mx-auto flex cursor-pointer items-center justify-center gap-0 rounded-full border-none bg-transparent px-0 py-5 font-normal shadow-none hover:bg-transparent [:hover,[data-pressed]]:bg-transparent">
               <span className="rounded-full bg-[#e1fcad] px-6 py-3 text-black duration-500 ease-in-out group-hover:bg-[#122023] group-hover:text-[#e1fcad] group-hover:transition-colors">
-                Get Started
+                <Link to="/get-started">
+                  Get Started
+                </Link>
               </span>
               <div className="relative flex h-fit cursor-pointer items-center overflow-hidden rounded-full bg-[#e1fcad] p-5 text-black duration-500 ease-in-out group-hover:bg-[#122023] group-hover:text-[#e1fcad] group-hover:transition-colors">
                 <ArrowUpRight className="absolute h-5 w-5 -translate-x-1/2 transition-all duration-500 ease-in-out group-hover:translate-x-10" />
