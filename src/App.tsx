a31b48ca52db89b9657a4b0b6f8bb32e220567f0
festus-sulumeti <sulumetifestus@outlook.com>
2026-07-23 19:05:50 +0300

    feat: Authentication importations

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
-import Hero from './components/HeroSection/Hero'
-import Login from './Pages/Login';
-import Signup from './Pages/Signup';
+import Hero from '@/components/HeroSection/Hero'
 
+
+/* Authentication Pages */ 
+import Login from '@/Pages/Login'
+import Signup from '@/Pages/Signup'
 
 
 function App() {
-  
-
   return (
    <>
+ 
     <Routes>
       <Route path="/" element={<Hero />} />
+
+      {/* Authentication Pages */}
       <Route path="/login" element={<Login />} />
       <Route path="/get-started" element={<Signup />} />
+    
+
     </Routes>
    </>
   )
